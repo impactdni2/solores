7f92c5c5a02150e00329ef8fb44ab381b45d1cd4
impactdni <impactdni@gmail.com>
2025-06-27 16:03:41 -0400

    raydium generated

---
+++ examples/anchor/raydium-cpmm/raydium_cp_swap_interface/src/typedefs.rs
@@ -0,0 +1,94 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use bytemuck::{Pod, Zeroable};
+use solana_program::pubkey::Pubkey;
+#[derive(Clone, Debug, BorshDeserialize, BorshSerialize, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct AmmConfig {
+    pub bump: u8,
+    pub disable_create_pool: bool,
+    pub index: u16,
+    pub trade_fee_rate: u64,
+    pub protocol_fee_rate: u64,
+    pub fund_fee_rate: u64,
+    pub create_pool_fee: u64,
+    pub protocol_owner: Pubkey,
+    pub fund_owner: Pubkey,
+    pub padding: [u64; 16],
+}
+#[derive(Clone, Debug, BorshDeserialize, BorshSerialize, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct LpChangeEvent {
+    pub pool_id: Pubkey,
+    pub lp_amount_before: u64,
+    pub token_0_vault_before: u64,
+    pub token_1_vault_before: u64,
+    pub token_0_amount: u64,
+    pub token_1_amount: u64,
+    pub token_0_transfer_fee: u64,
+    pub token_1_transfer_fee: u64,
+    pub change_type: u8,
+}
+#[repr(packed, C)]
+#[derive(Clone, Debug, BorshDeserialize, BorshSerialize, PartialEq, Pod, Copy, Zeroable)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct Observation {
+    pub block_timestamp: u64,
+    pub cumulative_token_0_price_x32: u128,
+    pub cumulative_token_1_price_x32: u128,
+}
+unsafe impl Pod for Observation {}
+unsafe impl Zeroable for Observation {}
+#[repr(packed, C)]
+#[derive(Clone, Debug, BorshDeserialize, BorshSerialize, PartialEq, Pod, Copy, Zeroable)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct ObservationState {
+    pub initialized: bool,
+    pub observation_index: u16,
+    pub pool_id: Pubkey,
+    pub observations: [Observation; 100],
+    pub padding: [u64; 4],
+}
+unsafe impl Pod for ObservationState {}
+unsafe impl Zeroable for ObservationState {}
+#[repr(packed, C)]
+#[derive(Clone, Debug, BorshDeserialize, BorshSerialize, PartialEq, Pod, Copy, Zeroable)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct PoolState {
+    pub amm_config: Pubkey,
+    pub pool_creator: Pubkey,
+    pub token_0_vault: Pubkey,
+    pub token_1_vault: Pubkey,
+    pub lp_mint: Pubkey,
+    pub token_0_mint: Pubkey,
+    pub token_1_mint: Pubkey,
+    pub token_0_program: Pubkey,
+    pub token_1_program: Pubkey,
+    pub observation_key: Pubkey,
+    pub auth_bump: u8,
+    pub status: u8,
+    pub lp_mint_decimals: u8,
+    pub mint_0_decimals: u8,
+    pub mint_1_decimals: u8,
+    pub lp_supply: u64,
+    pub protocol_fees_token_0: u64,
+    pub protocol_fees_token_1: u64,
+    pub fund_fees_token_0: u64,
+    pub fund_fees_token_1: u64,
+    pub open_time: u64,
+    pub recent_epoch: u64,
+    pub padding: [u64; 31],
+}
+unsafe impl Pod for PoolState {}
+unsafe impl Zeroable for PoolState {}
+#[derive(Clone, Debug, BorshDeserialize, BorshSerialize, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct SwapEvent {
+    pub pool_id: Pubkey,
+    pub input_vault_before: u64,
+    pub output_vault_before: u64,
+    pub input_amount: u64,
+    pub output_amount: u64,
+    pub input_transfer_fee: u64,
+    pub output_transfer_fee: u64,
+    pub base_input: bool,
+}
